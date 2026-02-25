94d39371fec485f7400f7270dd802e3acf0c599c
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-26 05:17:28 +0700

    Fix palindrome checker to handle input validation and fix bug

--- Minitask-1.js
+++ Minitask-1.js
@@ -1,9 +1,10 @@
 function polindromeChecker(str) {
   if (str === undefined || typeof str !== "string" || str === "") {
     return console.log("Input tidak valid");
+    const cleanData = str.toLowerCase().replace(/[^a-z0-9]/g);
   }
-  let strReverse = str.toLowerCase().split("").reverse().join("");
-  if (strReverse === str.toLowerCase()) {
+  let strReverse = cleanData.split("").reverse().join("");
+  if (strReverse === cleanData) {
     return console.log("str tersebut merupakan polindrome");
   } else {
     return console.log("str tersebut bukan merupakan polindrome");
