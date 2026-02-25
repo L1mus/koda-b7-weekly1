daccc2db9b65a71517a6c91a1473843f75c7bf76
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-25 22:20:37 +0700

    Add multiplication table generator function

---
+++ Minitask-2.js
@@ -0,0 +1,19 @@
+function genMultificationtable (num){
+  if(isNaN(num) || typeof num !=="number"){
+    return console.log("Gunakan tipe data number, selain itu tidak valid")
+  }
+    let result = ""
+    for(let i = 1 ; i <= num; i++){
+        // result =+ `${i}`
+        for(let j = 1; j<= num  ; j++){
+            result += `${i*j}`
+        }
+        result += "\n"
+    }
+    return console.log(result)
+}
+
+genMultificationtable(5)
+genMultificationtable("342432")
+genMultificationtable("hello world")
+genMultificationtable("!@#$%^&*")
