d09364a34fd91bd8c98336f24068f69e90f232a2
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-26 09:50:12 +0700

    Add examples for Array.some() and Array.every() methods

--- Minitask-4.js
+++ Minitask-4.js
@@ -16,3 +16,23 @@ melakukan perhitungan pada semua element element array yang bertipe number dan f
 
 const arrNums = [1,2,3,4,5]
 arrNums.reduce((a,b) => a+b,0)
+
+/*
+Array.some(callbackFn)
+Metode some()instance Array akan mengembalikan nilai true jika menemukan elemen dalam array yang memenuhi fungsi pengujian yang diberikan. Jika tidak, ia akan mengembalikan nilai false.
+*/
+
+const someDatas = [1,2,3,4,5,6,7,8,9,10];
+console.log(someDatas.some(value => value % 2 === 0));
+
+
+/*
+Array.every(callbackFn)
+Metode some()instance Array akan mengembalikan nilai true jika menemukan semua elemen dalam array yang memenuhi fungsi pengujian yang diberikan. Jika tidak, ia akan mengembalikan nilai false.
+*/
+
+const everyDatas = [2,4,6,8,10,12,14,16];
+
+console.log(someDatas.every(value=>value % 2 === 0))
+
+console.log(everyDatas.every(value=>value % 2 === 0))
