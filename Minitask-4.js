c3a5f81e30779c0a7ead89027349426cc47ef00b
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-26 09:29:13 +0700

    Add examples for string and array methods
    Add examples for String.replace and Array.reduce methods

--- Minitask-4.js
+++ Minitask-4.js
@@ -1,1 +1,18 @@
+/*
+String.replace(pattern,replacement)
+mengembalikan string baru dengan satu, beberapa, atau semua kecocokan dari pattern yang diganti dengan replacement 
+*/
+const sayHello = "Selamat Pagi!"
 
+console.log(sayHello.replace("Pagi","Siang"))
+
+/*
+array.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+melakukan perhitungan pada semua element element array yang bertipe number dan fungsi tersebut akan mengembalikan nilai berupa hasil perhitungan seluruh element dalam array
+*/
+
+const arrNums = [1,2,3,4,5]
+arrNums.reduce((a,b) => a+b,0)
