d4c2f52d9177578f5e9ce43d63af9a6f52317f64
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-27 09:38:12 +0700

    Implement countingToN function
    Add a function to count up to a specified number with intervals.

--- Minitask-5.js
+++ Minitask-5.js
@@ -1,1 +1,15 @@
+const countingToN = (n)=>{
+  if(typeof n !== "number"){
+    console.log("input invalid")
+  }
+  let result = 0
+    const interval= setInterval(()=>{
+       console.log(result += 1) 
+          if(result === n){
+            clearInterval(interval)
+          } 
+    }, 1000)
+}
 
+
+countingToN(6)
